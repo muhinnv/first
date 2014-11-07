53170ae501f463d9bcb743dc56294d69996ad6dc
ravil23 <ravilruzalevich@mail.ru>
2014-11-07 21:12:26 +0300

    Update views, controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,19 @@
 class PostsController < ApplicationController
+
+	before_action :post
+
 	def index
 		@posts = Post.all
 	end
+
+	def show
+		
+	end
+
+	private
+		def post
+			@post ||= Post.find(params[:id])
+		end
+
+		helper_method :post
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Создаем пользователя
+user = User.create(login: 'test_user_1', first_name: 'first_name_1', last_name: 'last_name_1')
+
+# Создаем посты, относящиеся к созданному пользователю
+user.posts.create(title: 'first_user_post_1', body: 'first_user_post_1_body')
+user.posts.create(title: 'first_user_post_2', body: 'first_user_post_2_body')
+user.posts.create(title: 'first_user_post_3', body: 'first_user_post_3_body')
+
+# Создаем комментарии от созданного пользователя, относящиеся к созданным постам
+user.posts.each do |post|
+	5.times do |i|
+		post.comments.create(title: "Title #{i+1}", message: "Message #{(i+1)*(i+1)}")
+	end
+end
