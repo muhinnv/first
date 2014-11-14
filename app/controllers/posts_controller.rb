7d0b1bbce005888d5b1307937cee496c84c2256f
ravil23 <ravilruzalevich@mail.ru>
2014-11-14 20:03:34 +0300

    Layouts and forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,5 @@
 class PostsController < ApplicationController
 
-	before_action :post
-
 	def index
 		@posts = Post.all
 	end
@@ -10,9 +8,24 @@ class PostsController < ApplicationController
 		
 	end
 
+	def new
+		@post = Post.new
+	end
+
+	def create
+		puts params
+		Post.create(post_params)
+		# flash[:notice] = "Post was succesfully created"
+		redirect_to posts_path, notice: "Post was succesfully created"
+	end
+
 	private
 		def post
 			@post ||= Post.find(params[:id])
+		end
+
+		def post_params
+			params.require(:post).permit(:title, :body)
 		end
 
 		helper_method :post
